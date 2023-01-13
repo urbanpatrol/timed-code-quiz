762fbcee2b6fbcf18140e84fc01c02cb2a7181c6
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-01-13 17:21:08 +0200

    Upload of initial files and draft set of questioins in questioins.js

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+// timer set to 75s
+// timer decreases by 15 for each incorrect answer
+// 5 questions with 4 alternative responses for each
+
+var startButton = document.getElementById("start-btn");
+
+// Start button triggers the first question and then displays the 'next' button
+
+
+
