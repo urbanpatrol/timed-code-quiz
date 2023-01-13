2acec118b5c2446c3d79a2f77d3f912565cddd59
urbanpatrol <33529607+urbanpatrol@users.noreply.github.com>
2023-01-13 20:00:16 +0200

    Updates to the logic.js file - lines 12-38 specific to the timer and start quiz button function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,41 @@
 // timer set to 75s
-// timer decreases by 15 for each incorrect answer
-// 5 questions with 4 alternative responses for each
+// timer decreases by 15 for each incorrect answer??
+// 5 questions with 4 answers, one true for each
 
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+// Time set to 75. Time left will also be the score.
+var timeLeft = 75;
+var timeId;
+
 
 // Start button triggers the first question and then displays the 'next' button
+// element.addEventListener("type-of-event", functionToExecute)
+// Listening for click event on Start Quiz button
+startButton.addEventListener("click", startQuiz);
+// Listening for click event on Next button ????
+nextButton.addEventListener("click", () => {
+    currentQuestionindex++
+    setNextQuestion()
+});
 
-startButton.addEventListener
+// Start quiz
+function startQuiz {
+timerId = setInterval(tickerTimer, 1000);
+
+}
+
+
+
+// Timer countdown
+function tickerTimer() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+// save score
+    }
+
+}
+
+
 
